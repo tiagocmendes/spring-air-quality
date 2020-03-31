6b4190d39385245933b2bee6da2711e45c16c118
tiagocmendes <tiagocmendes@ua.pt>
2020-03-31 16:25:47 +0100

    Simple REST Api setup

---
+++ airQuality/src/main/java/tqs/airQuality/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package tqs.airQuality.repository;
+
+import tqs.airQuality.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
