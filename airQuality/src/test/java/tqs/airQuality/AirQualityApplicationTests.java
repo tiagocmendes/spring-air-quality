5951faf2dbaadbb69149cb972adaeb3bba1d82f8
tiagocmendes <tiagocmendes@ua.pt>
2020-04-02 15:11:13 +0100

    Added dummy test

--- airQuality/src/test/java/tqs/airQuality/AirQualityApplicationTests.java
+++ airQuality/src/test/java/tqs/airQuality/AirQualityApplicationTests.java
@@ -3,11 +3,14 @@ package tqs.airQuality;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class AirQualityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
