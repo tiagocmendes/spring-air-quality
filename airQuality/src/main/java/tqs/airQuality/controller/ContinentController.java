8385086415c8203deb48e2ccc75f0a576c5f1f59
tiagocmendes <tiagocmendes@ua.pt>
2020-04-11 23:42:13 +0100

    Added country cache

--- airQuality/src/main/java/tqs/airQuality/controller/ContinentController.java
+++ airQuality/src/main/java/tqs/airQuality/controller/ContinentController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import tqs.airQuality.cache.Cache;
 import tqs.airQuality.model.Continent;
-import tqs.airQuality.model.Region;
+import tqs.airQuality.model.Country;
 import tqs.airQuality.repository.ContinentRepository;
-import tqs.airQuality.service.ContinentService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,6 +19,10 @@ public class ContinentController {
 
     @Autowired
     ContinentRepository continentRepository;
+
+    private static long TIME_TO_LIVE = 120;
+    private static long TIMER = 120;
+    private static Cache<String, Continent> regionCache = new Cache<>(TIME_TO_LIVE, TIMER);
 
     @GetMapping("/continents")
     public List<Continent> getAllContinents() {
--- airQuality/src/main/java/tqs/airQuality/controller/CountryController.java
+++ airQuality/src/main/java/tqs/airQuality/controller/CountryController.java
@@ -2,8 +2,10 @@ package tqs.airQuality.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tqs.airQuality.cache.Cache;
 import tqs.airQuality.model.Continent;
 import tqs.airQuality.model.Country;
+import tqs.airQuality.model.Region;
 import tqs.airQuality.repository.CountryRepository;
 
 import javax.validation.Valid;
@@ -16,24 +18,41 @@ public class CountryController {
     @Autowired
     CountryRepository countryRepository;
 
+    private static long TIME_TO_LIVE = 120;
+    private static long TIMER = 120;
+    private static Cache<String, List<Country>> regionCache = new Cache<>(TIME_TO_LIVE, TIMER);
+
     @GetMapping("/countries")
     public List<Country> getAllCountries() {
-        return countryRepository.findAll();
+        List<Country> allCountries = regionCache.get("allCountries");
+
+        if(allCountries == null) {
+            allCountries = countryRepository.findAll();
+            regionCache.put("allCountries", allCountries);
+        }
+
+        return allCountries;
     }
 
     @GetMapping("/countries/{continent}")
     public List<Country> getCountriesByContinent(@PathVariable(value = "continent") String continent) {
 
-        List<Country> countries = countryRepository.findAll();
+        List<Country> countriesByContinent = regionCache.get("countriesByContinent");
+        
+        if(countriesByContinent == null) {
+            List<Country> countries = countryRepository.findAll();
 
-        List<Country> returnCountries = new ArrayList<>();
-        for(Country c : countries) {
-            if(c.getContinent().toLowerCase().equals(continent.toLowerCase())) {
-                returnCountries.add(c);
+            countriesByContinent = new ArrayList<>();
+            for(Country c : countries) {
+                if(c.getContinent().toLowerCase().equals(continent.toLowerCase())) {
+                    countriesByContinent.add(c);
+                }
             }
+
+            regionCache.put("countriesByContinent", countriesByContinent);
         }
 
-        return returnCountries;
+        return countriesByContinent;
     }
 
     @PostMapping("/countries")
