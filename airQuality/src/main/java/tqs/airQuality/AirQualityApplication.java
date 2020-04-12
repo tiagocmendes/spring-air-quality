9490b53336e1c614668df4b15cc44d85b6fb5003
tiagocmendes <tiagocmendes@ua.pt>
2020-04-12 02:40:11 +0100

    Cors-headers done

--- airQuality/src/main/java/tqs/airQuality/AirQualityApplication.java
+++ airQuality/src/main/java/tqs/airQuality/AirQualityApplication.java
@@ -2,6 +2,9 @@ package tqs.airQuality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirQualityApplication {
@@ -9,5 +12,17 @@ public class AirQualityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirQualityApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("DELETE", "POST", "PUT", "GET");
+			}
+		};
+	}
+
 
 }
