70d091248ccc5a6b8ee975e1dbb0c1d7b4dbc0f6
tiagocmendes <tiagocmendes@ua.pt>
2020-04-13 23:14:06 +0100

    Contry repository tests

--- airQuality/src/main/java/tqs/airQuality/repository/CountryRepository.java
+++ airQuality/src/main/java/tqs/airQuality/repository/CountryRepository.java
@@ -3,9 +3,12 @@ package tqs.airQuality.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import tqs.airQuality.model.Continent;
 import tqs.airQuality.model.Country;
 
 import java.util.List;
 
 @Repository
-public interface CountryRepository extends JpaRepository<Country, Long> { }
+public interface CountryRepository extends JpaRepository<Country, Long> {
+    public Country findByName(String name);
+}
