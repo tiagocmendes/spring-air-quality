771863246ef2bbc406edaf326cc3dc78cc2812c7
tiagocmendes <tiagocmendes@ua.pt>
2020-04-14 23:13:44 +0100

    Added csv files

---
+++ data/test.py
@@ -0,0 +1,343 @@
+countries = [
+    {
+        "country": "Afghanistan",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Afghanistan.svg"
+    },
+    {
+        "country": "Albania",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/3/36/Flag_of_Albania.svg"
+    },
+    {
+        "country": "Algeria",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/7/77/Flag_of_Algeria.svg"
+    },
+    {
+        "country": "Andorra",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/19/Flag_of_Andorra.svg"
+    },
+    {
+        "country": "Argentina",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg"
+    },
+    {
+        "country": "Australia",
+        "continent": "OCEANIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/8/88/Flag_of_Australia_%28converted%29.svg"
+    },
+    {
+        "country": "Austria",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_Austria.svg"
+    },
+    {
+        "country": "Bangladesh",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/f9/Flag_of_Bangladesh.svg"
+    },
+    {
+        "country": "Belgium",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/92/Flag_of_Belgium_%28civil%29.svg"
+    },
+    {
+        "country": "Bolivia",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Bolivia.svg"
+    },
+    {
+        "country": "Brazil",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg"
+    },
+    {
+        "country": "Brunei",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Brunei.svg"
+    },
+    {
+        "country": "Bulgaria",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Bulgaria.svg"
+    },
+    {
+        "country": "Cambodia",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_Cambodia.svg"
+    },
+    {
+        "country": "Canada",
+        "continent": "NORTHERN AMERICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/c/cf/Flag_of_Canada.svg"
+    },
+    {
+        "country": "Chile",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/7/78/Flag_of_Chile.svg"
+    },
+    {
+        "country": "China",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fa/Flag_of_the_People%27s_Republic_of_China.svg"
+    },
+    {
+        "country": "Colombia",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg"
+    },
+    {
+        "country": "Croatia",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/1b/Flag_of_Croatia.svg"
+    },
+    {
+        "country": "Denmark",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9c/Flag_of_Denmark.svg"
+    },
+    {
+        "country": "Ecuador",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/e/e8/Flag_of_Ecuador.svg"
+    },
+    {
+        "country": "Ethiopia",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/7/71/Flag_of_Ethiopia.svg"
+    },
+    {
+        "country": "Finland",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Finland.svg"
+    },
+    {
+        "country": "France",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg"
+    },
+    {
+        "country": "Germany",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg"
+    },
+    {
+        "country": "Ghana",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/1/19/Flag_of_Ghana.svg"
+    },
+    {
+        "country": "Greece",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Greece.svg"
+    },
+    {
+        "country": "Grenada",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Grenada.svg"
+    },
+    {
+        "country": "Guatemala",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/e/ec/Flag_of_Guatemala.svg"
+    },
+    {
+        "country": "Guinea",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/e/ed/Flag_of_Guinea.svg"
+    },
+    {
+        "country": "Hungary",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/c/c1/Flag_of_Hungary.svg"
+    },
+    {
+        "country": "Iceland",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Iceland.svg"
+    },
+    {
+        "country": "India",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg"
+    },
+    {
+        "country": "Indonesia",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg"
+    },
+    {
+        "country": "Iran",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Flag_of_Iran.svg"
+    },
+    {
+        "country": "Ireland",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/45/Flag_of_Ireland.svg"
+    },
+    {
+        "country": "Italy",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg"
+    },
+    {
+        "country": "Jamaica",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/0/0a/Flag_of_Jamaica.svg"
+    },
+    {
+        "country": "Japan",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/9/9e/Flag_of_Japan.svg"
+    },
+    {
+        "country": "Laos",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/5/56/Flag_of_Laos.svg"
+    },
+    {
+        "country": "Luxembourg",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/d/da/Flag_of_Luxembourg.svg"
+    },
+    {
+        "country": "Macedonia",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/f8/Flag_of_Macedonia.svg"
+    },
+    {
+        "country": "Malaysia",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg"
+    },
+    {
+        "country": "Mali",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/92/Flag_of_Mali.svg"
+    },
+    {
+        "country": "Malta",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Malta.svg"
+    },
+    {
+        "country": "Mexico",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg"
+    },
+    {
+        "country": "Mongolia",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Mongolia.svg"
+    },
+    {
+        "country": "Nepal",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/9b/Flag_of_Nepal.svg"
+    },
+    {
+        "country": "Netherlands",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg"
+    },
+    {
+        "country": "Norway",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg"
+    },
+    {
+        "country": "Pakistan",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg"
+    },
+    {
+        "country": "Panama",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/a/ab/Flag_of_Panama.svg"
+    },
+    {
+        "country": "Peru",
+        "continent": "LATIN AMER. & CARIB",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Peru.svg"
+    },
+    {
+        "country": "Philippines",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_the_Philippines.svg"
+    },
+    {
+        "country": "Poland",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/1/12/Flag_of_Poland.svg"
+    },
+    {
+        "country": "Portugal",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg"
+    },
+    {
+        "country": "Romania",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg"
+    },
+    {
+        "country": "Serbia",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/ff/Flag_of_Serbia.svg"
+    },
+    {
+        "country": "Singapore",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg"
+    },
+    {
+        "country": "Slovakia",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/e/e6/Flag_of_Slovakia.svg"
+    },
+    {
+        "country": "Slovenia",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/f/f0/Flag_of_Slovenia.svg"
+    },
+    {
+        "country": "Spain",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/9/9a/Flag_of_Spain.svg"
+    },
+    {
+        "country": "Sweden",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg"
+    },
+    {
+        "country": "Switzerland",
+        "continent": "EUROPE",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg"
+    },
+    {
+        "country": "Thailand",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg"
+    },
+    {
+        "country": "Uganda",
+        "continent": "AFRICA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/4/4e/Flag_of_Uganda.svg"
+    },
+    {
+        "country": "Vietnam",
+        "continent": "ASIA",
+        "flag": "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Vietnam.svg"
+    }
+]
+
+
+with open('coutries.csv', 'w') as f:
+
+    for country in countries:
+        f.write(f"{country['country']},{country['continent']},{country['flag']}\n")
