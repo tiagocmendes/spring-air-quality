bcde2a04a46951eb07165458f87cdfd4fafa4399
tiagocmendes <tiagocmendes@ua.pt>
2020-04-15 21:25:01 +0100

    Uploaded screenshots

--- airQuality/src/test/java/tqs/airquality/service/RegionServiceTest.java
+++ airQuality/src/test/java/tqs/airquality/service/RegionServiceTest.java
@@ -1,9 +1,6 @@
 package tqs.airquality.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
