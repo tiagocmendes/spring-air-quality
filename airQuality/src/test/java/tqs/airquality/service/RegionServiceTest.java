63de2cc796843ba24b86fcec1ed5230e88e2a008
tiagocmendes <tiagocmendes@ua.pt>
2020-04-15 21:15:08 +0100

    Small change

--- airQuality/src/test/java/tqs/airquality/service/RegionServiceTest.java
+++ airQuality/src/test/java/tqs/airquality/service/RegionServiceTest.java
@@ -1,11 +1,11 @@
 package tqs.airquality.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith(MockitoExtension.class)
 public class RegionServiceTest {
-
 }
